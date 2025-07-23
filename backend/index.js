9abc731a75baf5ee95837a643c3b751d96b292d5
pooja8788 <139002646+pooja8788@users.noreply.github.com>
2025-07-23 20:13:23 +0530

    added become creator for user

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
