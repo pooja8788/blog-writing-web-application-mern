dab59071babc3aefc24ea316666757cb0d0e3f99
pooja8788 <139002646+pooja8788@users.noreply.github.com>
2025-07-26 11:51:27 +0530

    final commit

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,40 +1,5 @@
-// // eslint-disable-next-line no-unused-vars
-// import React from "react";
-// import Hero from "../Home/Hero";
-// import Trending from "../Home/Trending";
-// import Devotional from "../Home/Devotional";
-// import Creator from "../Home/Creator";
-// import Coding from "../Home/Coding";
-// import Entertainment from "../Home/Entertainment";
-// import Business from "../Home/Business";
-// import Sports from "../Home/Sports";
-// import Search from "../Home/Search";
-
-
-
-
-
-// function Home() {
-  
-//   return (
-//     <div>
-//       <Search />
-//       <Hero />
-//       <Trending />
-//       <Entertainment />
-//       <Sports />
-//       <Coding />      
-//       <Business />
-//       <Devotional />
-//       <Creator />
-//     </div>
-//   );
-// }
-
-// export default Home;
-
-
-import { useEffect } from "react";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import Hero from "../Home/Hero";
 import Trending from "../Home/Trending";
 import Devotional from "../Home/Devotional";
@@ -45,26 +10,12 @@ import Business from "../Home/Business";
 import Sports from "../Home/Sports";
 import Search from "../Home/Search";
 
+
+
+
+
 function Home() {
-  useEffect(() => {
-    // Restore scroll position on load
-    const savedScrollY = localStorage.getItem("scrollY");
-    if (savedScrollY) {
-      window.scrollTo(0, parseInt(savedScrollY, 10));
-    }
-
-    // Save scroll position before unload
-    const handleBeforeUnload = () => {
-      localStorage.setItem("scrollY", window.scrollY);
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
+  
   return (
     <div>
       <Search />
@@ -72,7 +23,7 @@ function Home() {
       <Trending />
       <Entertainment />
       <Sports />
-      <Coding />
+      <Coding />      
       <Business />
       <Devotional />
       <Creator />
