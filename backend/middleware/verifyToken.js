534b8733599e391d36a42e6c0bdb0b9bd37e1d67
pooja8788 <139002646+pooja8788@users.noreply.github.com>
2025-08-05 23:41:07 +0530

    new category feature added

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.model.js';
+import { User } from '../models/user.model.js';
 
 const verifyToken = async (req, res, next) => {
   const token = req.cookies.token; 
