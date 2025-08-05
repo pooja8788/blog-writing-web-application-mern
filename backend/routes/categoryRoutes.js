c7fd12c80ef0566796e95ba8ae826a259dcf8562
pooja8788 <139002646+pooja8788@users.noreply.github.com>
2025-08-05 23:51:26 +0530

    new category feature added

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getCategories, addCategory, deleteCategory } from '../controller/categoryController.js';
-import {verifyToken} from '../middleware/verifyToken.js';
+import verifyToken from '../middleware/verifyToken.js';
 import { isSuperAdmin } from '../middleware/isSuperAdmin.js';
 
 const router = express.Router();
