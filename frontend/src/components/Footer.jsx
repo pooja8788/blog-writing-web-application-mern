d6eef45ebc8826a4c157910702872b8a1bd8e3bf
pooja8788 <139002646+pooja8788@users.noreply.github.com>
2025-08-15 01:19:01 +0530

    VIEW BLOG

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -43,8 +43,8 @@ import { FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <footer className="bg-gray-600 py-1">
-        <div className="bg-gray-600 py-4">
+      <footer className="bg-gray-400 py-1">
+        <div className="bg-gray-400 py-4">
         <div className="container mx-auto flex flex-col items-center space-y-4">
           <div className="text-xl font-semibold">
             Swar<span className="text-blue-500 font-bold">Lekhan</span>
@@ -54,7 +54,7 @@ const Footer = () => {
           </div>
           <div className="flex space-x-4">
             <a href="#">
-              <FaGithub className="h-6 hover:text-gray-900" />
+              <FaGithub className="h-6 hover:text-black-500" />
             </a>
             <a href="#">
               <BsYoutube className="h-6 hover:text-red-600" />
