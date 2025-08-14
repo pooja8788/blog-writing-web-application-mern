bdbe7e3e20c2a27c765b00eba413226e4b814d55
pooja8788 <139002646+pooja8788@users.noreply.github.com>
2025-08-15 01:11:36 +0530

    VIEW BLOG

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,41 @@
-// import React from "react";
+// // import React from "react";
+// import { FaGithub } from "react-icons/fa";
+// import { BsYoutube } from "react-icons/bs";
+// import { FaLinkedin } from "react-icons/fa";
+
+// const Footer = () => {
+//   return (
+//     <>
+//       <footer className="border py-10">
+//       </footer>
+//       <div className=" container mx-auto  flex flex-col md:flex-row justify-between items-center">
+//         <div className="text-xl font-semibold hidden md:flex">
+//           Swar<span className="text-blue-500 font-bold">Lekhan</span>
+//         </div>
+//         <div className="text-gray-400 text-sm hidden md:flex">
+//           <p>&copy; 2025 SwarLekhan. All rights reserved</p>
+//         </div>
+//         <div className="mt-4 md:mt-0 flex space-x-4">
+//           <a href="#">
+//             <FaGithub className="h-6" />
+//           </a>
+//           <a href="#">
+//             <BsYoutube className="h-6" />
+//           </a>
+
+//           <a href="#">
+//             <FaLinkedin className="h-6" />
+//           </a>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Footer;
+
+
+
 import { FaGithub } from "react-icons/fa";
 import { BsYoutube } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
@@ -6,28 +43,31 @@ import { FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <footer className="border py-10">
-      </footer>
-      <div className=" container mx-auto  flex flex-col md:flex-row justify-between items-center">
-        <div className="text-xl font-semibold hidden md:flex">
-          Swar<span className="text-blue-500 font-bold">Lekhan</span>
-        </div>
-        <div className="text-gray-400 text-sm hidden md:flex">
-          <p>&copy; 2025 SwarLekhan. All rights reserved</p>
-        </div>
-        <div className="mt-4 md:mt-0 flex space-x-4">
-          <a href="#">
-            <FaGithub className="h-6" />
-          </a>
-          <a href="#">
-            <BsYoutube className="h-6" />
-          </a>
-
-          <a href="#">
-            <FaLinkedin className="h-6" />
-          </a>
+      <footer className="bg-blue-800 py-10">
+        <div className="bg-gray-300 py-4">
+        <div className="container mx-auto flex flex-col items-center space-y-4">
+          <div className="text-xl font-semibold">
+            Swar<span className="text-blue-500 font-bold">Lekhan</span>
+          </div>
+          <div className="text-gray-500 text-sm text-center">
+            <p>&copy; 2025 SwarLekhan. All rights reserved</p>
+          </div>
+          <div className="flex space-x-4">
+            <a href="#">
+              <FaGithub className="h-6 hover:text-gray-900" />
+            </a>
+            <a href="#">
+              <BsYoutube className="h-6 hover:text-red-600" />
+            </a>
+            <a href="#">
+              <FaLinkedin className="h-6 hover:text-blue-700" />
+            </a>
+          </div>
         </div>
       </div>
+      </footer>
+
+      
     </>
   );
 };
